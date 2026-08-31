89e3f437a88f2f947759b69cac047b9e2e9f883b
4ndres-316 <andresmelna33@gmail.com>
2026-08-31 13:43:59 -0400

    first commit

---
+++ gestion_veterinaria/Pages/Citas.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace gestion_veterinaria.Pages
+{
+    public class CitasModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ gestion_veterinaria/Pages/Mascotas.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace gestion_veterinaria.Pages
+{
+    public class MascotasModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
